6d1dc6999561237209923d632a9495c05ccc6f28
lxg <x773182338@163.com>
2020-03-20 10:47:11 +0800

    分支修改

--- app/src/main/java/com/polyclinic/gittest/MainActivity.java
+++ app/src/main/java/com/polyclinic/gittest/MainActivity.java
@@ -3,7 +3,11 @@ package com.polyclinic.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Lxg
+ * @create 2020/3/20
+ * @Describe 测试用例
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
